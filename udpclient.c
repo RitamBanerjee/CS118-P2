a3c395343299dd8aeaf83aa18bec840b92c19457
Ritam Banerjee <Ritam@Suvros-MacBook-Pro.local>
2017-03-04 14:37:14 -0800

    Starter Code

---
+++ udpclient.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+  int clientSocket, portNum, nBytes;
+  char buffer[1024];
+  struct sockaddr_in serverAddr;
+  socklen_t addr_size;
+  if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+  }
+
+  clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
+
+  portNum = atoi(argv[1]);
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(portNum);
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+
+  /*Initialize size variable to be used later on*/
+  addr_size = sizeof serverAddr;
+
+  while(1){
+    printf("Type a sentence to send to server:\n");
+    fgets(buffer,1024,stdin);
+    printf("You typed: %s",buffer);
+
+    nBytes = strlen(buffer) + 1;
+    
+    /*Send message to server*/
+    sendto(clientSocket,buffer,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
+
+    /*Receive message from server*/
+                    nBytes = recvfrom(clientSocket,buffer,1024,0,NULL, NULL);
+
+    printf("Received from server: %s\n",buffer);
+
+  }
+
+  return 0;
+}
